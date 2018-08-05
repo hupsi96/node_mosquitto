fe315071a995f1a5b91e75d9dcfe01ffb6b78d25
Christian Huppertz <christian.huppertz@tum.de>
2018-08-05 16:50:02 +0200

    adjust mqtt settings

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var mqtt = require('mqtt')
 var client = mqtt.connect([{ host: '192.168.2.114', port: 1883 }])
 
@@ -5,8 +7,8 @@ client.on('connect', function () {
     client.subscribe('zimmer/#')
   })
 
-  client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString())
-    client.end()
-  })
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
