004680f6bcaaf9517418cb77a9a8cfd4cdcec906
Christian Huppertz <christian.huppertz@tum.de>
2018-08-05 17:16:42 +0200

    Testing connection function

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var mqtt = require('mqtt')
-var client = mqtt.connect([{ host: '192.168.2.114', port: 1883 }])
+var client = mqtt.connect('mqtt://192.168.2.114')
 
 client.on('connect', function () {
   console.log("Client is connected to 192.168.2.114:1883")
