a237596ad70603964b84e0f2acebc0fdcf51008d
Christian Huppertz <christian.huppertz@tum.de>
2018-08-05 17:05:54 +0200

    modify broker ip

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var mqtt = require('mqtt')
-var client = mqtt.connect([{ host: '192.168.2.114', port: 1883 }])
+var client = mqtt.connect('192.168.2.114')
 
 client.on('connect', function () {
     client.subscribe('zimmer/#')
