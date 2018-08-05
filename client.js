7c9e52631af970d3ab164c6859a7c5ae1e333ddb
Christian Huppertz <christian.huppertz@tum.de>
2018-08-05 17:08:23 +0200

    add string output

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 'use strict'
 
 var mqtt = require('mqtt')
-var client = mqtt.connect('192.168.2.114')
+var client = mqtt.connect([{ host: '192.168.2.114', port: 1883 }])
 
 client.on('connect', function () {
+  console.log("Client is connected to 192.168.2.114:1883")
     client.subscribe('zimmer/#')
   })
 
